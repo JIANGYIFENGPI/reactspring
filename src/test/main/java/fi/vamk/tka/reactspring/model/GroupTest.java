859d2e40c64456c6e9d439d6d7d24ea1fade961f
Jiang <e1601134@edu.vamk.fi>
2019-04-05 15:06:32 +0300

    added database test

---
+++ src/test/main/java/fi/vamk/tka/reactspring/model/GroupTest.java
@@ -0,0 +1,14 @@
+package fi.vamk.tka.reactspring.model;
+
+public class GroupTest {
+    @GroupTest
+    public void justTesting() {
+        System.out.println("----------test,test,test-------------");
+    }
+
+    public void againTesting() {
+        System.out.println("------------timo,timo,timo------------");
+
+    }
+
+}
